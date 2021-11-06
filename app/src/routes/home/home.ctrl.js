ddb4b18e86360fe87a23952b4a5a88896d83da30
LCY <stat_chae0@sookmyung.ac.kr>
2021-11-07 01:24:49 +0900

    로그인 화면 꾸미기

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStroage");
+const User = require("../../models/User");
 
 const output = {
     hello: (req, res)=>{
@@ -13,24 +13,28 @@ const output = {
 
 const process = {
     login: (req, res)=>{
-        const id = req.body.id,
-            password  = req.body.password;
-        
-        const users = UserStorage.getUsers("id", "password");
-        const response = {};
-        if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.password[idx] === password){
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        response.success = false;
-        response.message = "로그인에 실패하셨습니다";
-
+        const user = new User(req.body);
+        const response = user.login();
+        console.log(response);
         return res.json(response);
+        // const id = req.body.id,
+        //     password  = req.body.password;
+        
+        // const users = UserStorage.getUsers("id", "password");
+        // const response = {};
+        // if(users.id.includes(id)){
+        //     const idx = users.id.indexOf(id);
+        //     if(users.password[idx] === password){
+        //         response.success = true;
+        //         return res.json(response);
+        //     }
+        // }
+        // response.success = false;
+        // response.message = "로그인에 실패하셨습니다";
+
+        // return res.json(response);
     }
-}
+};
 
 module.exports = {
     output,
